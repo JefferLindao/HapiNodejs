b9b3bf52db95cb5a23550f2c2675f891a49c7a96
JefferLindao <jefferlindao@gmail.com>
2020-12-06 23:38:01 -0500

    El objeto h, response y sus herramientas

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ async function init() {
     method: 'GET',
     path: '/',
     handler: (req, h) => {
-      return 'Hola mundo! JefferLindao'
+      return h.response('Hola mundo ...').code(200)
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/redirect',
+    handler: (req, h) => {
+      return h.redirect('http://platzi.com')
     }
   })
 
