7b47905a9cb7c770d9b65a806bc9657382907625
Jeffer Lindao <jefferlindao@gmail.com>
2021-03-10 15:07:00 -0500

    Validando informacion - Implementando Joi

--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
-'use strict';
+'use strict'
+
+const Joi = require('@hapi/joi')
 const site = require('./controllers/site')
 const user = require('./controllers/user')
 module.exports =
@@ -13,13 +15,20 @@ module.exports =
     path: '/register',
     handler: site.register
   },
-
   {
     method: 'POST',
+    options: {
+      validate:{
+        payload: Joi.object({
+          name: Joi.string().required().min(3),
+          email: Joi.string().email().required(),
+          password: Joi.string().required().min(6)
+        })
+      }
+    },
     path: '/create-user',
     handler: user.createUser
   },
-
   {
     method: 'GET',
     path: '/{param*}',
