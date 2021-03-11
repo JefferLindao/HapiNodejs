7772cdf9f08ae11a23be420addfd98ba1d47bfe7
JefferLindao <jefferlindao@gmail.com>
2021-03-10 22:36:45 -0500

    Creando modelo y guardado en firebase

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,15 @@
 'use strict'
+const users = require('../models/index').users
 
-function createUser(req, h){
-  console.log(req.payload)
-  return 'Usuario Creado'
+async function createUser(req, h) {
+  let result
+  try {
+    result = await users.create(req.payload)
+  } catch (error) {
+    console.error(error)
+    return h.response('Problemas creando el usuario').code(500)
+  }
+  return h.response(`Usuario creado ID: ${result}`)
 }
 
 module.exports = {
