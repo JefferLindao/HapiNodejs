f7c6f377e7474714a143dac5dfcaf8085f624ce2
JefferLindao <jefferlindao@gmail.com>
2021-03-17 20:35:22 -0500

    Usando Metodos del servidor

--- controllers/question.js
+++ controllers/question.js
@@ -3,6 +3,9 @@
 const questions = require('../models/index').questions
 
 async function createQuestion(req, h) {
+  if (!req.state.user) {
+    return h.redirect('/login')
+  }
   let result
   try {
     result = await questions.create(req.payload, req.state.user)
@@ -18,6 +21,9 @@ async function createQuestion(req, h) {
 }
 
 async function answerQuestion(req, h) {
+  if (!req.state.user) {
+    return h.redirect('/login')
+  }
   let result
   try {
     result = await questions.answer(req.payload, req.state.user)
@@ -28,7 +34,21 @@ async function answerQuestion(req, h) {
   return h.redirect(`/question/${req.payload.id}`)
 }
 
+async function setAnswerRight(req, h) {
+  if (!req.state.user) {
+    return h.redirect('/login')
+  }
+  let result
+  try {
+    result = await req.server.methods.setAnswerRight(req.params.questionId, req.params.answerId, req.state.user)
+    console.log(result)
+  } catch (error) {
+    console.error(error)
+  }
+  return h.redirect(`/question/${req.params.questionId}`)
+}
 module.exports = {
   createQuestion: createQuestion,
-  answerQuestion: answerQuestion
+  answerQuestion: answerQuestion,
+  setAnswerRight: setAnswerRight
 }
